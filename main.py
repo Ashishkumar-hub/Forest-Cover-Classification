b0f6aeccfad4f5acfd9b4611eafdb0aeac081f88
Ashish Kumar <55132850+Ashishkumar-hub@users.noreply.github.com>
2022-02-15 16:43:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def trainRouteClient():
 #    app.run(host = '127.0.0.1',port=8080,debug=True)
 port = int(os.getenv("PORT",5001))
 if __name__ == "__main__":
-    host = '127.0.0.1'
+    host = '0.0.0.0'
     #port = 5000
     httpd = simple_server.make_server(host, port, app)
     # print("Serving on %s %d" % (host, port))
