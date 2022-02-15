ee11aa0da4533af2e4efa6c0e88f59f1e356c7c4
Ashish Kumar <55132850+Ashishkumar-hub@users.noreply.github.com>
2022-02-15 18:08:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def trainRouteClient():
     return Response("Training successfull!!")
 
 if __name__ == "__main__":
-    app.run(host = '127.0.0.1',port=8080,debug=True)
+    app.run(host = '0.0.0.0',port=80,debug=True)
 
 #port = int(os.getenv("PORT", 5000))
 #if __name__ == "__main__":
