001d395d2083a5658cd2270099b64903411f1cb1
Ashish Kumar <55132850+Ashishkumar-hub@users.noreply.github.com>
2022-02-15 18:07:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -91,13 +91,13 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successfull!!")
 
-
-port = int(os.getenv("PORT", 5000))
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    # port = 5000
-    httpd = simple_server.make_server(host, port, app)
-    # print("Serving on %s %d" % (host, port))
-    httpd.serve_forever()
+    app.run(host = '127.0.0.1',port=8080,debug=True)
+
+#port = int(os.getenv("PORT", 5000))
 #if __name__ == "__main__":
-#    app.run(host = '127.0.0.1',port=8080,debug=True)
+#    host = '0.0.0.0'
+    # port = 5000
+#    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+#    httpd.serve_forever()
